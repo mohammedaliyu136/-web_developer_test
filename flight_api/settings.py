c807c5fd6e8b30c60a60804c8f4648986d737b1d
kabir <mohammedaliyu136@gmail.com>
2019-11-28 18:25:13 +0100

    initial commit

--- flight_api/settings.py
+++ flight_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7ogzka-*qztb1zi3*^36i=j$&9^m(%&$dzak&uk-f7mwayc*(i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
